0ab935293cb0b7eaa1ad276093edf9e420f41bc3
Ryan Chen <113121132+RyanChen979@users.noreply.github.com>
2024-10-23 10:47:08 -0400

    kms

--- src/DB/mongoose.js
+++ src/DB/mongoose.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 require("dotenv").config({ path: ".env" });
 
-mongoose.connect(
-  `${process.env.MONGO_URI}` /* , {
+mongoose.connect(`${process.env.MONGO_URI}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-} */
-);
+});
 mongoose.connection.on("error", (err) => {
   console.error(`${err.message}`);
 });
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,19 @@
 require("dotenv").config({ path: ".env" });
-const test = require("../models/test");
+const sample_supplies = require("../models/test");
 
 exports.auth = async (req, res, next) => {
   try {
-    const data = test;
+    /* const data = sample_supplies.forEach((e) => {
+      e.sales;
+    }); */
 
-    res.json(data);
+    const salesData = [];
+    sample_supplies.forEach((e) => {
+      salesData.push(e.sales);
+    });
+
+    /* res.json(salesData); */
+    return salesData;
   } catch (error) {
     res.json(
       "verification failed. kevins french kiss denied. kevins special spanish spanking being delivered"
