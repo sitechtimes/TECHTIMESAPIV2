d9793b7dbf908a9f1f0b670f6ca9f13a36659340
Ryan Chen <113121132+RyanChen979@users.noreply.github.com>
2024-11-14 11:02:26 -0500

    kms

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import cookieParser from "cookie-parser"; // Middleware for parsing cookies
-import createError from "http-errors"; // Middleware for creating HTTP errors
-import express from "express"; // Express web framework
-import logger from "morgan"; // HTTP request logger middleware
-import path from "path"; // Utility for working with file and directory paths
-import routes from "./routes/index";
+const cookieParser = require("cookie-parser"); // Middleware for parsing cookies
+const createError = require("http-errors"); // Middleware for creating HTTP errors
+const express = require("express"); // Express web framework
+const logger = require("morgan"); // HTTP request logger middleware
+const path = require("path"); // Utility for working with file and directory paths
+const routes = require("./routes/index");
 
 require("./DB/mongoose");
 let app = express(); // Create an instance of the Express application
@@ -77,4 +77,4 @@ app.use((err, req, res, next) => {
   res.render("error"); // Render the error view
 });
 
-export default { app };
+module.exports = app;
