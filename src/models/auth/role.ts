2852f0ecd9f0fc4ea5e7142a611b065265e2819d
lawrence <91681777+whydoesthecodenotwork@users.noreply.github.com>
2024-11-17 19:29:18 -0500

    nuke haha

--- src/models/auth/role.ts
+++
@@ -1,5 +0,0 @@
-export enum Role {
-  Writer = "writer",
-  Editor = "editor",
-  Admin = "admin",
-}
--- src/models/cms/position.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Position = void 0;
-var Position;
-(function (Position) {
-    Position["main"] = "main";
-    Position["sub"] = "sub";
-    Position["detail"] = "detail";
-})(Position || (exports.Position = Position = {}));
