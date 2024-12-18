a8082565b85da1baa2199372b1bde33ba5b6ac81
lawrence <91681777+whydoesthecodenotwork@users.noreply.github.com>
2024-12-18 10:20:06 -0500

    password longer

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -12,8 +12,8 @@ router.post(
     body("email").isEmail().withMessage("Email must be valid"),
     body("password")
       .trim()
-      .isLength({ min: 8, max: 16 })
-      .withMessage("Password must be between 8 and 16 characters"),
+      .isLength({ min: 8, max: 24 })
+      .withMessage("Password must be between 8 and 24 characters"),
   ],
   validateRequest,
   authController.signUp
